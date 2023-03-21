b8d2a52904d3476a53f7166d41ac26bf55aaf598
Lucas Soares <drak263@gmail.com>
2023-03-21 19:16:22 -0300

    Exception handling - delete

--- src/main/java/com/lucas/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lucas/course/resources/exceptions/ResourceExceptionHandler.java
@@ -2,11 +2,13 @@ package com.lucas.course.resources.exceptions;
 
 import java.time.Instant;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.lucas.course.services.exceptions.DatabaseException;
 import com.lucas.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +20,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DatabaseException e, HttpServletRequest request) {
+		String error = "Data Integrity error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
